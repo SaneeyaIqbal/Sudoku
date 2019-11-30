c9046f5208348beb5df794d94467ec0e11af25f8
SaneeyaIqbal <s.saneeya.iqbal@gmail.com>
2019-11-30 12:17:55 +0530

    Sudoku game is completed

--- sudoku/src/App.js
+++ sudoku/src/App.js
@@ -1,37 +1,120 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
 class Box extends Component {
+  constructor(props) {
+    super(props);
+    this.state =
+      {
+        count :1,
+        color: '#ccc',
+        disabled: false
+      };
+  }
+  answer(useranswer){
+
+    if(this.props.answer === useranswer)
+    {
+      this.setState(
+        {
+          color:'#ccc',
+          disabled :true
+        }
+      )
+    }
+   else if(this.props.answer !== useranswer && useranswer!==''){
+    this.props.wrongCount();
+      if(this.state.count === 1)
+      {
+        this.setState(
+          {
+            color:'blue',
+            count:2
+          }
+        )
+      }
+      else if(this.state.count === 2)
+      {
+        this.setState(
+          {
+            color:'red',
+            count:3
+          }
+        )
+      }
+      else if(this.state.count === 3)
+      {
+        this.setState(
+          {
+            color:'black',
+            count:4,
+            disabled :true
+          }
+        )
+      }
+    }
+      }
+  render(){
+      return (
+        <div className='grid-box'>
+          {this.props.number ? (
+            <div className='Box'> {this.props.number}</div>
+          ) : (
+                  <input type='text' value={this.state.value} style={{backgroundColor:this.state.color}}
+                  onChange={(event)=> this.answer(event.target.value)} disabled={(this.state.disabled)? 'disabled':''} maxLength={1} />
+                )
+          }
+        </div>
+      )
+    }
+}
+class App extends Component {
   constructor(props){
     super(props);
-      this.state={};
+    this.state={
+      incorrectAttempts:0
+    }
+    this.increment = this.increment.bind(this);
   }
 
 
-  render() {
+  increment (){
+    this.setState({
+      incorrectAttempts: this.state.incorrectAttempts + 1
+    })
+    }
+  
+    render() {
     return (
-      <div>
-        {this.props.number ? (
-          <div> Box has number {this.props.number}</div>
-        ) : (
-          <input type="text"/>
-        )}
+      <div className="grid">
+        <div className="grid-line">
+          <div className="Box-row">
+            <Box number={3} />
+            <Box answer={1} wrongCount={this.increment} />
+            <Box answer={4} wrongCount={this.increment} />
+            <Box number={2} />
+          </div> 
+          <div className="Box-row">
+            <Box answer={2} wrongCount={this.increment} />
+            <Box number={4} />
+            <Box number={1} />
+            <Box answer={3} wrongCount={this.increment} />
+          </div>
+          <div className="Box-row">
+            <Box answer={1} wrongCount={this.increment} />
+            <Box number={3} />
+            <Box number={2} />
+            <Box answer={4} wrongCount={this.increment} />
+          </div>
+          <div className="Box-row">
+            <Box number={4} />
+            <Box answer={2} wrongCount={this.increment} />
+            <Box answer={3} wrongCount={this.increment} />
+            <Box number={1} />
+          </div>
+        </div>
+        <h1> Wrong Attempts={this.state.incorrectAttempts}</h1>
       </div>
-     
-    )
+    );
   }
 }
-
-class App extends Component {
-  render() {
-    return(
-      <div className="App">
-        <Box number={2} />
-        <Box answer={2} />
-      </div> 
-       )
-  }
-}
-
 export default App;
